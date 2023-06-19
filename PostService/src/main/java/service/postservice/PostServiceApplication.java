7920628a1f419f717bf47ffc167f6843061a1044
valentinbujdoso <valentinbujdoso@gmail.com>
2023-06-19 11:17:16 -0500

    Fixes

--- PostService/src/main/java/service/postservice/PostServiceApplication.java
+++ PostService/src/main/java/service/postservice/PostServiceApplication.java
@@ -2,12 +2,14 @@ package service.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PostServiceApplication {
 
     public static void main(String[] args) {
